ef9e5b3109dae90f838f4637b8842288f791413c
Cristian-VP <cvacacelaprocel@cifpfibmoll.eu>
2024-10-11 19:39:57 +0200

    Primer commit. Ejercios 0-4

---
+++ salida_datos_por_pantalla/EX01-02.java
@@ -0,0 +1,18 @@
+/**
+ * Ejercicio 1
+ * Escribe una programa que muestre tu nombre por pantalla.
+ *
+ * Ejercicio 2
+ * Modifica el programa anterior para que además se muestre tu dirección y
+ * tu número de teléfono. Asegúrate de que los datos se muestran en líneas
+ * separadas
+ */
+
+public class Main{
+    public static void main(String[], arg){
+        System.out.printf("%-5s World!\n", "Hello");
+        System.out.printf("%-4s %4s I have %d years ols\n", "I Am", "Tian", 15);
+        System.out.printf("My phone number: %d \\u{1F618}\n",);
+        System.out.printf("\\u{1F377} My adress %4s number %d", "tree \\u{1F377}", 4)
+    }
+}
---
+++ salida_datos_por_pantalla/EX03-04.java
@@ -0,0 +1,15 @@
+/**
+ * Ejercicio 1
+ * Escribe un programa que muestre tu horario de clase
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hora        Lunes         Martes        Miercoles       Jueves       Viernes");
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.printf("%-4.2f_%4.2f %-12s %12s %12s %12s %12s\n",15.30, 16.30 ,"Programacion", "Programacion", "Programacion", "Programacion", "Programacion");
+        System.out.printf("%-4.2f_%4.2f %-12s %12s %12s %12s %12s\n",16.30, 17.30 ,"Programacion", "Programacion", "Programacion", "Programacion", "Programacion");
+        System.out.printf("%-4.2f_%4.2f %-12s %12s %12s %12s %12s\n",18.30,  19.30,"Programacion", "Programacion", "Programacion", "Programacion", "Programacion");
+        System.out.printf("%-4.2f_%4.2f %-12s %12s %12s %12s %12s\n",19.30,  20.30,"Programacion", "Programacion", "Programacion", "Programacion", "Programacion");
+        System.out.printf("%-4.2f_%4.2f %-12s %12s %12s %12s %12s\n",20.30,  21.30,"Programacion", "Programacion", "Programacion", "Programacion", "Programacion");
+    }
+}
